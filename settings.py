3bfc665c88e0c8bdfb9fc23a94ac0134c163a633
Michael Arakilian <arakilian0@gmail.com>
2019-07-03 23:41:27 -0700

    ADDED a settings file for modular calculator configurations, and more(added license & gitignore)

---
+++ settings.py
@@ -0,0 +1,20 @@
+screen = {
+    "bg": "blue",
+    "rows": 0,
+    "columns": 0,
+    "columnspan": 4,
+    "padx": 5,
+    "pady": 5,
+}
+
+input = {
+    "bg": "blue",
+    "fg": "red",
+    "fs": "20px",
+}
+
+button = {
+    "bg": "blue",
+    "fg": "red",
+    "fs": "20px",
+}
